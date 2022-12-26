95cf7222a2a4105b0e94529b76a739b40e3f524c
Rizal Rahmadani <rizalbnt121@gmail.com>
2022-12-26 19:36:07 +0700

    DetailTiketActivity.kt, edit profile

---
+++ app/src/main/java/com/rzl/flightgotiketbooking/view/activity/DetailTiketActivity.kt
@@ -0,0 +1,12 @@
+package com.rzl.flightgotiketbooking.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rzl.flightgotiketbooking.R
+
+class DetailTiketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_tiket)
+    }
+}
