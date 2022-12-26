dc97d1e9daaf815d60da25a4c999c14de7e76e7b
ibal.libul <ibal.libul>
2022-12-26 19:56:09 +0700

    coba

--- app/src/main/java/com/rzl/flightgotiketbooking/view/activity/AvailableFlightActivity.kt
+++ app/src/main/java/com/rzl/flightgotiketbooking/view/activity/AvailableFlightActivity.kt
@@ -16,26 +16,6 @@ class AvailableFlightActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_available_flight)
 
-//        binding.toolbarBack.setOnClickListener{
-//            startActivity(Intent(this@AvailableFlightActivity, MainActivity::class.java) )
-//        }
-
-//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-//        supportActionBar!!.title="Available Flight"
-
-
-        //Data available flight
-//        val listFlight = arrayListOf(
-//            DataFlight(R.drawable.garuda,"RP 1000.000"),
-//            DataFlight(R.drawable.garuda,"RP 1000.000"),
-//            DataFlight(R.drawable.garuda,"RP 1000.000"),
-//            DataFlight(R.drawable.garuda,"RP 1000.000"),
-//        )
-//        var adapterFlight = AdapterFlight(listFlight)
-//        val viewFlight = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-//        binding.rvAvalaibleFlights.layoutManager = viewFlight
-//        binding.rvAvalaibleFlights.adapter = adapterFlight
 
     }
 }
